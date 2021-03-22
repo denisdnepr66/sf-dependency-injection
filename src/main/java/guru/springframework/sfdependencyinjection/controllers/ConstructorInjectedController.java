c5e6dc7ad7e11c981798dc2d9cd1b86caeeb0320
denisdnepr66 <56921743+denisdnepr66@users.noreply.github.com>
2021-03-22 15:45:12 +0200

    dependency injection with Spring

--- src/main/java/guru/springframework/sfdependencyinjection/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfdependencyinjection/controllers/ConstructorInjectedController.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfdependencyinjection.controllers;
 
 import guru.springframework.sfdependencyinjection.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
