16079746e01e76b2237798c1f87bcc600d7fec65
denisdnepr66 <56921743+denisdnepr66@users.noreply.github.com>
2021-03-22 15:53:35 +0200

    qualifiers added

--- src/test/java/guru/springframework/sfdependencyinjection/controllers/SetterInjectedControllerTest.java
+++ src/test/java/guru/springframework/sfdependencyinjection/controllers/SetterInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package guru.springframework.sfdependencyinjection.controllers;
 
-import guru.springframework.sfdependencyinjection.services.GreetingServiceImpl;
+import guru.springframework.sfdependencyinjection.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterInjectedControllerTest {
 
@@ -13,7 +11,7 @@ class SetterInjectedControllerTest {
     @BeforeEach
     void setUp() {
         controller = new SetterInjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test
