d81b227ea5993b279c6b27f2deef78957742b8ac
denisdnepr66 <56921743+denisdnepr66@users.noreply.github.com>
2021-03-28 14:18:02 +0300

    application properties modification

--- src/main/java/guru/springframework/sfdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfdependencyinjection/config/GreetingServiceConfig.java
@@ -7,9 +7,11 @@ import guru.springframework.sfdependencyinjection.repositories.EnglishGreetingRe
 import guru.springframework.sfdependencyinjection.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfdependencyinjection.services.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
 
